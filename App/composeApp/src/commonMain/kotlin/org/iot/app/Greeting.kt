f607d9fd62b0cfc41daa4104b4e21ba07916de08
dcfrenci <dcfrenci@gmail.com>
2026-03-23 23:32:34 +0100

    App comunicating with API

--- App/composeApp/src/commonMain/kotlin/org/iot/app/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package org.iot.app
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- App/composeApp/src/commonMain/kotlin/org/iot/app/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.iot.app
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
